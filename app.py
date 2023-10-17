ec58bbd959e1c1826526dac5c2f9fc716ad9c970
Zinobili <zinofung0628@gmail.com>
2023-10-18 01:04:34 +0800

    FEATURE: adding view result option after the proess and basic result view

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, jsonify
 from flask_socketio import SocketIO, emit
 import time
 
+## reading csv result
+import pandas as pd
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
@@ -10,14 +13,22 @@ socketio = SocketIO(app)
 def index():
     return render_template('landing.html')
 
+@app.route('/result')
+def result():
+    file_path='./static/working_file/mockup.csv'
+    df=pd.read_csv(file_path)
+    result=csv_to_list_of_dicts(file_path)
+    print(result)
+    return render_template('landing.html',result=result)
 
-@app.route("/start", methods=["POST"])
+
+@app.route("/analyze", methods=["POST"])
 def start_process():
     # Start the long-running process here
     # This may involve running a separate function or spawning a new thread
     
     # For demonstration purposes, let's sleep for 10 seconds
-    time.sleep(10)
+    time.sleep(5)
 
     # Notify the frontend that the process is complete
     socketio.emit("process_complete", {"message": "Processing task finished"})
@@ -34,6 +45,19 @@ def get_progress():
     progress = random.randint(0, 100)
     
     return jsonify({"progress": progress})
+
+
+import csv
+
+def csv_to_list_of_dicts(file_path):
+    data = []
+    
+    with open(file_path, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    
+    return data
 
 
 if __name__ == '__main__':
